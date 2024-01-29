1ab0c4aeb6397ea6ecaf9ab4f1c251c5f9dcd3db
aldara <aldarafernandezcarro@gmail.com>
2024-01-29 10:47:00 +0100

    Se añade la ruta al home

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import './App.css'
 import Carousel from './components/Carousel';
 import FeaturedProducts from './components/FeaturedProducts';
+import Footer from './components/Footer';
 import NavBar from './components/NavBar';
-
+import {Navigate, Route, Routes, BrowserRouter} from "react-router-dom";
 
 const App = () => <>
-  <NavBar />
-  <Carousel />
-  <FeaturedProducts />
+  <BrowserRouter>
+    <NavBar />
+    <Routes>
+      <Route path=''>
+        <Route path='/' element={<Navigate to="/home" />} />
+        <Route path='/home' element={<>
+          <Carousel />
+          <FeaturedProducts />
+          </>} />
+      </Route>
+    </Routes>
+    <Footer />
+  </BrowserRouter>
 </>
 
 
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+const Footer = () => <>
+    <footer className="bg-body-tertiary text-center text-lg-start">
+        <div className="text-center p-3">
+            © 2024 Aldara Fernandez
+    </div>
+</footer>
+</>
+
+export default Footer;
