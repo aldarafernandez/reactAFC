b2ee496aa2a2cdee218d7c3e83dcbb9dbf8ce1a4
aldara <aldarafernandezcarro@gmail.com>
2024-01-29 11:59:23 +0100

    Se agrupan los componentes del home en otro componente

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+import Carousel from "../Carousel";
+import FeaturedProducts from "../FeaturedProducts";
+import "./Home.css";
+
+const Home = () => <>
+    <Carousel />
+    <FeaturedProducts />
+</>
+
+export default Home;
