1cbebfe16e4b4b4d3d2932a036e66f72783e94ec
aldara <aldarafernandezcarro@gmail.com>
2024-03-02 11:39:22 +0100

    Se añade un layout para poder mostrar el navbar y el footer junto con las rutas

---
+++ src/components/layout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+
+
+export default function Layout(){
+
+    return (
+        <>
+            <NavBar />
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
+        </>
+    )
+}
