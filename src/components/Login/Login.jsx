49468f12910cc301141ba6b4e080e03dadac6b6b
aldara <aldarafernandezcarro@gmail.com>
2024-03-02 12:05:23 +0100

    Se maneja el envío del formulario usando la función login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,20 +1,40 @@
+import { useAuth } from "../../Provider/authProvider";
 import "./Login.css";
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Login = () => {
 
-    return <form className="container d-flex flex-column justify-content-center align-items-center mb-5 login">
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+    const {login} = useAuth();
+
+    const handleSubmit = (event) => {
+
+        event.preventDefault();
+        login(username, password);
+    }
+
+    return <form className="container d-flex flex-column justify-content-center align-items-center mb-5 login" onSubmit={handleSubmit}>
         <h2 className="align-self-start mb-5">Login</h2>
         <div className="input-group mb-4">
             <span className="input-group-text" id="basic-addon1">@</span>
-            <input type="text" className="form-control" placeholder="Nombre de ususario" aria-label="Username" aria-describedby="basic-addon1" />
+            <input type="text" value={username} className="form-control" placeholder="Nombre de ususario" aria-label="Username" 
+            onChange={
+                (e) => setUsername(e.target.value)
+            }
+            />
         </div>
 
         <div className="input-group mb-4">
-            <input type="password" className="form-control" placeholder="Contraseña" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" />
+            <input type="password" value={password} className="form-control" placeholder="Contraseña" aria-label="Sizing example input"
+                onChange={
+                    (e) => setPassword(e.target.value)
+                } />
         </div>
 
         <div class="col-12 d-flex justify-content-end">
-            <button class="btn btn-outline-dark me-2 registerbtn" type="submit">Registrarse</button>
+            <button class="btn btn-outline-dark me-2 registerbtn"><NavLink to="/register">Registrarse</NavLink></button>
             <button class="btn btn-dark submitbtn" type="submit">Enviar</button>   
         </div>
 
