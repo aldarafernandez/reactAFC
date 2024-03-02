c27923d2ec2cdef84040814de7bab18d76e5d8d2
aldara <aldarafernandezcarro@gmail.com>
2024-03-02 11:39:48 +0100

    Se añaden las rutas

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import { useAuth } from "../Provider/authProvider"
 import Cart from "../components/Cart";
 import Home from "../components/Home";
@@ -7,42 +7,44 @@ import Product from "../components/Product/Product";
 import Products from "../components/Products";
 import Register from "../components/Register";
 import { RouterProvider } from "react-router";
+import Layout from "../components/layout";
+import { Children } from "react";
 
 
 const Routes = () => {
 
-    const {token} = useAuth();
+    const { token } = useAuth();
 
     const routesForPublic = [
-        {
-            path: "/",
-            element: <Navigate to="/home" />,
-        },
-        {
-            path: "/home",
-            element: <Home />,
-        },
-        {
-            path: "/products",
-            element: <Products />,
-        },
-        {
-            path: "/product/:id",
-            element: <Product />,
-        },
-        {
-            path: "/register",
-            element: <Register />,
-        },
-        {
-            path: "/login",
-            element: <Login />,
-        },
-        {
-            path: "*",
-            element: <Navigate to="/home" />,
-        },
-    ];
+            {
+                path: "/",
+                element: <Navigate to="/home" />,
+            },
+            {
+                path: "/home",
+                element: <Home />,
+            },
+            {
+                path: "/products",
+                element: <Products />,
+            },
+            {
+                path: "/product/:id",
+                element: <Product />,
+            },
+            {
+                path: "/register",
+                element: <Register />,
+            },
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "*",
+                element: <Navigate to="/home" />,
+            },
+        ];
 
     const routesForAuthenticatedOnly = [
         {
@@ -52,8 +54,13 @@ const Routes = () => {
     ];
 
     const router = createBrowserRouter([
-        ...routesForPublic,
-        ...Cart(token ? routesForAuthenticatedOnly : [])
+        {
+            element: <Layout />,
+            children: [
+                ...routesForPublic,
+                ...(token ? routesForAuthenticatedOnly : [])
+            ],
+        },
     ]);
 
     return <RouterProvider router={router} />
