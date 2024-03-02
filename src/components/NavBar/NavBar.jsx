8c7c6a700aeb5c640dbdcbc5e5d179ff6e39522a
aldara <aldarafernandezcarro@gmail.com>
2024-03-02 11:40:42 +0100

    Se modifican los enlaces

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,17 +3,17 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => <nav className="navbar navbar-expand-lg d-flex flex-row border-bottom">
     <div className="container-fluid d-flex justify-content-between">
-        <h1><NavLink to={"/home"}>Home</NavLink></h1>
+        <h1><NavLink to="/home">Home</NavLink></h1>
         <ul className="d-flex align-items-center ">
-            <li><NavLink to={"/products"}>Products</NavLink></li>
+            <li><NavLink to="/products">Products</NavLink></li>
             <li>
                 <form className="d-flex" role="search">
                     <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Buscar" />
                     <button className="btn btn-outline-dark" type="submit">Buscar</button>
                 </form>
             </li>
-            <li><NavLink to={"/register"}>Registrarse</NavLink></li>
-            <li><NavLink to={"/login"}>login</NavLink></li>
+            <li><NavLink to="/register">Registrarse</NavLink></li>
+            <li><NavLink to="/login">login</NavLink></li>
         </ul>
     </div>
 
