9ef704b6c7c90e79d4361ce11512b056680fdec6
aldara <aldarafernandezcarro@gmail.com>
2024-03-03 11:58:03 +0100

    Se manda al home después de hacer login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,28 +1,32 @@
 import { useAuth } from "../../Provider/authProvider";
 import "./Login.css";
 import { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Navigate } from 'react-router-dom';
+
 
 const Login = () => {
 
     const [username, setUsername] = useState();
     const [password, setPassword] = useState();
-    const {login} = useAuth();
+    const { login } = useAuth();
 
     const handleSubmit = (event) => {
 
         event.preventDefault();
         login(username, password);
+        if (localStorage.getItem("token")) {
+            <Navigate to={"/home"} />
+        }
     }
 
     return <form className="container d-flex flex-column justify-content-center align-items-center mb-5 login" onSubmit={handleSubmit}>
         <h2 className="align-self-start mb-5">Login</h2>
         <div className="input-group mb-4">
             <span className="input-group-text" id="basic-addon1">@</span>
-            <input type="text" value={username} className="form-control" placeholder="Nombre de ususario" aria-label="Username" 
-            onChange={
-                (e) => setUsername(e.target.value)
-            }
+            <input type="text" value={username} className="form-control" placeholder="Nombre de ususario" aria-label="Username"
+                onChange={
+                    (e) => setUsername(e.target.value)
+                }
             />
         </div>
 
@@ -35,7 +39,7 @@ const Login = () => {
 
         <div class="col-12 d-flex justify-content-end">
             <button class="btn btn-outline-dark me-2 registerbtn"><NavLink to="/register">Registrarse</NavLink></button>
-            <button class="btn btn-dark submitbtn" type="submit">Enviar</button>   
+            <button class="btn btn-dark submitbtn" type="submit">Enviar</button>
         </div>
 
     </form>
