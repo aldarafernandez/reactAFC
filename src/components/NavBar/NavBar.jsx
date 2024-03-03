d111771a50e896193275719e60b65acd0b4e9400
aldara <aldarafernandezcarro@gmail.com>
2024-03-03 11:57:29 +0100

    Se añaden las rutas condicionales en función del token

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,22 +1,35 @@
+import { useAuth } from "../../Provider/authProvider";
 import "./NavBar.css";
 import { NavLink } from 'react-router-dom';
 
-const NavBar = () => <nav className="navbar navbar-expand-lg d-flex flex-row border-bottom">
-    <div className="container-fluid d-flex justify-content-between">
-        <h1><NavLink to="/home">Home</NavLink></h1>
-        <ul className="d-flex align-items-center ">
-            <li><NavLink to="/products">Products</NavLink></li>
-            <li>
-                <form className="d-flex" role="search">
-                    <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Buscar" />
-                    <button className="btn btn-outline-dark" type="submit">Buscar</button>
-                </form>
-            </li>
-            <li><NavLink to="/register">Registrarse</NavLink></li>
-            <li><NavLink to="/login">Login</NavLink></li>
-        </ul>
-    </div>
+const NavBar = () => {
 
-</nav>
+    const { token, logout } = useAuth();
+
+
+    return <nav className="navbar navbar-expand-lg d-flex flex-row border-bottom">
+        <div className="container-fluid d-flex justify-content-between">
+            <h1><NavLink to="/home">Home</NavLink></h1>
+            <ul className="d-flex align-items-center ">
+                <li><NavLink to="/products">Products</NavLink></li>
+                <li>
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Buscar" />
+                        <button className="btn btn-outline-dark" type="submit">Buscar</button>
+                    </form>
+                </li>
+                {!token ? (<>
+                    <li><NavLink to="/register">Registrarse</NavLink></li>
+                    <li><NavLink to="/login">Login</NavLink></li>
+                </>)
+                    : (<>
+                        <li><NavLink to="/cart">Carrito</NavLink></li>
+                        <li><NavLink to="/logout" onClick={logout}>Cerrar Sesión</NavLink></li>
+                    </>)
+                }
+            </ul>
+        </div>
+    </nav>
+}
 
 export default NavBar;
