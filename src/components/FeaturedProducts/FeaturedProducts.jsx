3fb587a00b5ef9c23881523ec09100c037984181
aldara <aldarafernandezcarro@gmail.com>
2024-03-06 15:18:07 +0100

    Se añade el enlace para acceder al componente de detalle del producto

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,39 +1,53 @@
+import { NavLink } from "react-router-dom";
 import "./Featuredproducts.css";
 
-const FeaturedProducts = ({products}) => {
+const FeaturedProducts = ({ products }) => {
 
-    if(products.length === 0) {
+    if (products.length === 0) {
         return <p>No hay productos</p>
     }
 
-return <div className="container text-center" id="grid">
-    <div className="row">
-        <div className="col-4">
-            <div className="row h300">
-                <img src={products[0].image} alt={products[0].name} />
-            </div>
-            <div className="row h300">
-                <img src={products[1].image} alt={products[1].name} />
-            </div>
-            <div className="row h300">
-                <img src={products[2].image} alt={products[2].name} />
-            </div>
-        </div>
-        <div className="col-8">
-            <div className="row h600">
-                <img src={products[3].image} alt={products[3].name} />
-            </div>
-            <div className="row h600">
-                <div className="h300">
-                    <img src={products[4].image} alt={products[4].name} />
+    return <div className="container text-center" id="grid">
+        <div className="row">
+            <div className="col-4">
+                <div className="row h300">
+                    <NavLink to={`/product/${products[0].id}`}>
+                        <img src={products[0].image} alt={products[0].name} />
+                    </NavLink>
                 </div>
-                <div className="h300">
-                    <img src={products[5].image} alt={products[5].name} />
+                <div className="row h300">
+                    <NavLink to={`/product/${products[1].id}`}>
+                        <img src={products[1].image} alt={products[1].name} />
+                    </NavLink>
+                </div>
+                <div className="row h300">
+                    <NavLink to={`/product/${products[2].id}`}>
+                        <img src={products[2].image} alt={products[2].name} />
+                    </NavLink>
+                </div>
+            </div>
+            <div className="col-8">
+                <div className="row h600">
+                    <NavLink to={`/product/${products[3].id}`}>
+                        <img src={products[3].image} alt={products[3].name} />
+                    </NavLink>
+                </div>
+                <div className="row h600">
+                    <div className="h300">
+                        <NavLink to={`/product/${products[4].id}`}>
+                            <img src={products[4].image} alt={products[4].name} />
+                        </NavLink>
+                    </div>
+                    <div className="h300">
+                        <NavLink to={`/product/${products[5].id}`}>
+                            <img src={products[5].image} alt={products[5].name} />
+                        </NavLink>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>}
+}
 
 
 export default FeaturedProducts;
